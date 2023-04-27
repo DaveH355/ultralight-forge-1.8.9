8bd704623a7c1aedbd30d837431d7ba0c19c85ee
antoineok <antoineok@live.fr>
2023-04-27 15:15:44 +0200

    removed the dll from inside of the jar and make them download from the official repo, to make sure the mod is also working on mac and linux

--- src/main/java/examplemod/client/ultralight/UltraLight.java
+++ src/main/java/examplemod/client/ultralight/UltraLight.java
@@ -1,14 +1,18 @@
 package examplemod.client.ultralight;
 
+import com.labymedia.ultralight.UltralightJava;
 import com.labymedia.ultralight.UltralightLoadException;
 import com.labymedia.ultralight.UltralightPlatform;
 import com.labymedia.ultralight.UltralightRenderer;
 import com.labymedia.ultralight.config.FontHinting;
 import com.labymedia.ultralight.config.UltralightConfig;
+import com.labymedia.ultralight.gpu.UltralightGPUDriverNativeUtil;
+import com.labymedia.ultralight.os.OperatingSystem;
 import examplemod.client.utils.ResourceManager;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
 
 public class UltraLight {
     private static UltralightRenderer renderer;
@@ -17,9 +21,27 @@ public class UltraLight {
     public static void init() {
         try {
             ResourceManager.loadUltralight();
+
+            String[] libs = new String[] {
+                    "glib-2.0-0",
+                    "gobject-2.0-0",
+                    "gmodule-2.0-0",
+                    "gio-2.0-0",
+                    "gstreamer-full-1.0",
+                    "gthread-2.0-0"
+            };
+            Path natives = ResourceManager.binDir.toPath();
+            OperatingSystem os = OperatingSystem.get();
+            for(String lib : libs) {
+                System.load(natives.resolve(os.mapLibraryName(lib)).toAbsolutePath().toString());
+            }
+            UltralightJava.load(natives);
+            UltralightGPUDriverNativeUtil.load(natives);
         } catch (URISyntaxException | UltralightLoadException | IOException e) {
             throw new RuntimeException(e);
         }
+
+
 
         UltralightPlatform platform = UltralightPlatform.instance();
         platform.setConfig(
